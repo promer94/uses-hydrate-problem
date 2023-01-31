bde3f555affcc0193343c7fc88ab53ea1d8e24c8
Yixuan Xu <yixuanxu94@outlook.com>
2023-01-31 21:35:03 +0800

    add pages dir example

---
+++ app/error.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function ErrorComp() {
+  return <div>error</div>
+}
--- pages/pages-dir.tsx
+++ pages/pages-dir.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { useSyncExternalStore } from 'react'
 import { getClientSnapshot, getServerSnapshot, sub } from '~/lib/state'
 
